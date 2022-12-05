6be3148af37e8842f9547703c6dc9d2c8f5dcd7e
rohit3602 <100509084+rohit3602@users.noreply.github.com>
2022-12-05 22:33:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
     <div className="App">
       Hello World!
     </div>
+    <p>My name is Rohit Raj. I am a Third Year Undergraduate student of the department of Civil Engineering enrolled in its Dual Degree course. I am a boarder of Radhakrishnan Hall of Residence. 
+    </p>
   );
 }
 
