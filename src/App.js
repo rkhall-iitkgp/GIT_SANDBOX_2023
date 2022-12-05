78d80e418264ffcce5afd4c9664de3ce14cdf5fe
Adarsh-51 <108488189+Adarsh-51@users.noreply.github.com>
2022-12-05 21:24:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello World!
+//       Hello World!
+    <p>
+    Adarsha Mahapatra
+    </p>
+    <p> 
+    My name is Adarsha Mahapatra. 
+    I am second year undergraduate student of the department of chemical engineering enrolled in its B-tech course. 
+    I am a boarder of Radhakrishnan Hall of Residence. 
+    I am from Sunabeda, Odisha. 
+    </p>
     </div>
   );
 }
