381551d5bc876524ac1bc8a5202f9105ad43aee1
Ayush Biswas <biswas.ayush03@kgpian.iitkgp.ac.in>
2022-12-05 19:46:18 +0530

    intro added to app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <p>
+        My name is Ayush Biswas.
+      </p>
+      <p>
+        I am a second year undergraduate student of the department of Mathematics enrolled in it's B.S. course of Mathematics and Computing.
+      </p> 
+      <p>
+        I am a boarder of radhakrishnan hall of residence.
+      </p> 
+      <p>
+        I am from Chittorgarh, Rajasthan.
+      </p>
     </div>
   );
 }
