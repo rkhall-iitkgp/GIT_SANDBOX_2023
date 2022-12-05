e9488876c70d686b084f775d4cc76e18902099af
Daksh Varshney <80913975+DakshVarshney1409@users.noreply.github.com>
2022-12-05 19:56:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="App">
       Hello World!
+      <p>Name: Daksh Varshney</p>
+      <p>Dept.: Electronics and Electrical Communication Engineering</p>
+      <p>Place: Noida, UP</p>
     </div>
   );
 }
