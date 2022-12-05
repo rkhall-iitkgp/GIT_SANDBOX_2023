bbabb97fb48b8c0be3bcfa2fc168cc454983adcd
Anish Datta <anishd3139@gmail.com>
2022-12-06 00:56:08 +0530

    removed extra spaces

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ function App() {
       <p>Anish Datta</p>
       <p>I am a second year undergraduate student in the department of Computer Science and Engineering enrolled in its Dual Degree course. I am a boarder of Radhakrishnan Hall of Residence, and I am from Kolkata, West Bengal.</p>
     </div>
-    
-
   );
 }
 
