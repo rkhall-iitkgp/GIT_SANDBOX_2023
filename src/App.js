3c1f4e9f994797dc9a0d32557f49be06b17d24de
harshnigam45 <114400568+harshnigam45@users.noreply.github.com>
2022-12-06 10:00:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <p>Harsh Nigam<p>
-    <p> My name is Harsh Nigam. I am a third year undergrauduate student of the department of Electrical Engineering enrolled in its Dual Degree Course.I am from Varanasi,Uttar Pradesh.</p>
+    <p> My name is Harsh Nigam. I am a third year undergrauduate student of the department of Electrical Engineering enrolled in its Dual Degree Course.I am from Varanasi,Uttar Pradesh.My Hobbies are playing Badmintona and Basketball.</p>
     </div>
   );
 }
