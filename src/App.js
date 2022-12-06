cf63e666d647c0be88034106113a6fb562671f81
RKiteSandip <119930084+RKiteSandip@users.noreply.github.com>
2022-12-06 14:55:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ function App() {
   return (
     <div className="App">
       Hello World!
+      <p> Hello my name is Sandip Singha. <br>
+          I am a 2nd year undergraduate student in
+          IIT Kharagpur enrolled in its 4 years 
+          B.Tech course in the department of 
+          Aerospace Engineering and am a boarder of
+          Radhakrishnan Hall of residence.
+      </p>
     </div>
   );
 }
