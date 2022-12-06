77904b35962ee9075f8f543c9ecbbad6da7991c8
AbhishekDotasara <105533351+AbhishekDotasara@users.noreply.github.com>
2022-12-06 13:56:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello World!
+     <p> My name is Abhishek Dotasara. I am a third year undergraduate student of the department of Biotechnology enrolled in its dual degree course.I am from Sikar Rajasthan.</p>
+     
     </div>
   );
 }
