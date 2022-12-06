4b5aa53d13edd04293c5b02978254d2520a748f4
Yash23kr <100074099+Yash23kr@users.noreply.github.com>
2022-12-06 13:50:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="App">
       Hello World!
+    <p>
+    My name is Yash Kumar. I am 2nd-year undergraduate student of the Department of Computer Science and Engineering enrolled in its Dual Degree course. I am a boarder of Radhakrishnan Hall of Residance. I am from Jamshedpur, Jharkhand.
+    </p>
     </div>
   );
 }
