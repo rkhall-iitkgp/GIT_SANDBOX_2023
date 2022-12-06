baa148e0cb2d2921ff0acfebae039797a3f93b73
Rohit2901Gupta <108726323+Rohit2901Gupta@users.noreply.github.com>
2022-12-06 13:09:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <p> Hello, My name is Rohit Gupta. I am a third year
+    undergraduate student of the department of electrical 
+    engineering enrolled in its B.Tech course. I am from Gwalior,
+    Madhaya Pradesh. My hobbies include playing cricket and chess </p>
     </div>
   );
 }
