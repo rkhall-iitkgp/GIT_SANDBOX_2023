369c527275a97a7fee169e25afe0e224afe11279
Vrihad Anand Srivastav <77293816+vrihad7267@users.noreply.github.com>
2022-12-06 14:19:03 +0530

    Added name and intro: Vrihad

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="App">
       Hello World!
+        <p>
+          My name is Vrihad Anand Srivastav. I am a third year undergraduate student of the Department of Electrical Engineering enrolled in its BTech course. I am a border of Radhakrishnan hall of residence. I am from Gorakhpur, Uttar Pradesh. 
+        </p>
     </div>
   );
 }
