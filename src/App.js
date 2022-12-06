be36a5f7475fd99864e22bb0c1767a5ecb5428f3
anuraganand92 <98257282+anuraganand92@users.noreply.github.com>
2022-12-06 16:02:21 +0530

    update app.js with intro

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="App">
       Hello World!
+      <p>
+        My name is Anurag Anand. I am 2nd-year undergraduate student of the Department of Metallurgical and Materials Engineering enrolled in its BTech course. I am a boarder of Radhakrishnan Hall of Residance. I am from Jamshedpur, Jharkhand.
+      </p>
     </div>
   );
 }
