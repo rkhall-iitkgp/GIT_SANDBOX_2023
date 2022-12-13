19d2d17dbc039372c371b4e1e7fd6bd99922d856
Vasu Garg <88959104+VasuGarg2004@users.noreply.github.com>
2022-12-13 19:54:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <p>
+        My name is Vasu Garg. I am a second year undergraduate student of the department of Mechanical Engineering enrolled in its BTech course. I am a boarder of Radhakrishnan Hall of Residence. I am from Hyderabad, Telangana.
+      </p>
     </div>
   );
 }
