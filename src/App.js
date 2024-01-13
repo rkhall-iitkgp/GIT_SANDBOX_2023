7b08d29f4c3d0331be4291310146d1ee92c7184c
VaibhavTc <chakolevaibhav17@gmail.com>
2024-01-13 19:32:13 +0530

    FIrst commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <p>Hey there i am vaibhav</p>
+      <p>
+        I am Vaibhav Chakole. I am a second year student of the department of
+        chemistry enrolled in its B.S course. I am a boarder of radhakrishnan
+        hall of residence. I am from Bhandara Maharashtra.
+      </p>
     </div>
   );
 }
