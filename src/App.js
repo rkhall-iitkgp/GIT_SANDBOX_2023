45cd171a3b56cc4a7d4d00034a80701b21101d8d
Adarsh <adarshtadiparthi30@gmail.com>
2024-01-13 16:58:29 +0530

    Added my details

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="App">
       Hello World!
+      <p>Adarsh tadiparthi</p>
+      <p>My name is Adarsh Tadiparthi.I am a second year undergraduate student of the department of Electrical engineering enrolled in its B.Tech course. I am a boarder of Radhakrishnan Hall of residence.I am from Vijayawada, Andhrapradesh.</p>
     </div>
   );
 }
